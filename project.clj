be455828d66b9e4ce1fa75dd9de0a5260be92bd2
Ruth Linehan <ruth@puppetlabs.com>
2015-04-01 16:52:16 -0700

    Add repl utils
    Now, the app can be run from the repl by doing: `lein repl` and then `(go)`.

--- project.clj
+++ project.clj
@@ -7,17 +7,23 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :pedantic? :abort
+
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.2.0"]
+                 [compojure "1.2.0" :exclusions [clj-time]]
                  [cheshire  "5.3.1"]
                  [org.clojure/tools.logging "0.3.1"]
                  [puppetlabs/trapperkeeper ~tk-version]
                  [puppetlabs/trapperkeeper-webserver-jetty9 ~tk-jetty9-version]]
 
-  :profiles {:dev {:dependencies [[puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test"]
+  :profiles {:dev {:source-paths  ["dev"]
+                   :dependencies [[puppetlabs/trapperkeeper ~tk-version :classifier "test" :scope "test"]
                                   [puppetlabs/kitchensink ~ks-version :classifier "test" :scope "test"]
                                   [clj-http "0.9.2"]
+                                  [org.clojure/tools.namespace "0.2.4"]
                                   [ring-mock "0.1.5"]]}}
+
+  :repl-options {:init-ns user}
 
   :aliases {"tk" ["trampoline" "run" "--config" "dev-resources/config.conf"]}
 
