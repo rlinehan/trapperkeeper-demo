217e5f57edc106708fc462650050e03264d3de7f
Ruth Linehan <ruth@puppetlabs.com>
2015-04-19 21:48:36 -0700

    Namespace with-app-with-config

--- test/trapperkeeper_demo/trapperkeeper_demo_service_test.clj
+++ test/trapperkeeper_demo/trapperkeeper_demo_service_test.clj
@@ -1,7 +1,7 @@
 (ns trapperkeeper-demo.trapperkeeper-demo-service-test
   (:require [clojure.test :refer :all]
             [puppetlabs.trapperkeeper.app :as app]
-            [puppetlabs.trapperkeeper.testutils.bootstrap :refer [with-app-with-config]]
+            [puppetlabs.trapperkeeper.testutils.bootstrap :as tk-testutils]
             [trapperkeeper-demo.trapperkeeper-demo-service :as svc]))
 
 (deftest meow-services-test
@@ -10,7 +10,7 @@
                              ["miaou" svc/meow-french-service]
                              ["nyaa" svc/meow-japanese-service]]]
 
-    (with-app-with-config app [service]
+    (tk-testutils/with-app-with-config app [service]
       {:meow-service {:db-path "resources/test-meowdb.json"}}
       (let [meow-service (app/get-service app :MeowService)]
         (is (= (str "=^.^= < " sound " foo") (svc/meow meow-service "foo"))))))))
